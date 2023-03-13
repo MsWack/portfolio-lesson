5396052cba368da15bee074f423e727403c20098
Eleonora <sergeeva.nora@inbox.ru>
2023-03-13 18:20:00 +0300

    sections about&resume

--- src/js/script.js
+++ src/js/script.js
@@ -3,16 +3,30 @@
 //создаем переменные
 const hamburger = document.querySelector('.hamburger'),
 	menu = document.querySelector('.menu'),
-	closeElem = document.querySelector('.menu__close');
+	closeElem = document.querySelector('.menu__close'),
+	overlay = document.querySelector('.menu__overlay');
 
-//при клике на гамбургер будет выполняться фнукиця
+//при клике на гамбургер
 hamburger.addEventListener('click', () => {
-	//добавляем блоку меню класс активности
+	//добавляем блоку 'меню' класс активности
 	menu.classList.add('active');
 });
 
 //при клике на крестик
 closeElem.addEventListener('click', () => {
-	//убирвем у блока меню класс активности
+	//убираем у блока 'меню' класс активности
 	menu.classList.remove('active');
+});
+
+//закрываем блок 'меню' при клике на фон
+overlay.addEventListener('click', () => {
+	menu.classList.remove('active');
+});
+
+//закрываем блок 'меню' при клике на клавишу Esc (keyup - функция сработает, когда клавиша esc будет отпущена)
+document.addEventListener('keyup', function (e) {
+	if (e.key === "Escape") {
+		menu.classList.remove('active');
+	}
+	
 });
